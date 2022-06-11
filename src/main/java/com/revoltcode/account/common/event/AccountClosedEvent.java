95897c764fbef128a326bf05a26036bc125c83fc
oddy-bassey <oddybassey@gmail.com>
2022-06-11 22:51:22 +0100

    implemented account events

---
+++ src/main/java/com/revoltcode/account/common/event/AccountClosedEvent.java
@@ -0,0 +1,12 @@
+package com.revoltcode.account.common.event;
+
+import com.revoltcode.cqrs.core.event.BaseEvent;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@SuperBuilder
+public class AccountClosedEvent extends BaseEvent {
+}
---
+++ src/main/java/com/revoltcode/account/common/event/AccountOpenedEvent.java
@@ -0,0 +1,21 @@
+package com.revoltcode.account.common.event;
+
+import com.revoltcode.account.common.dto.AccountType;
+import com.revoltcode.cqrs.core.event.BaseEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class AccountOpenedEvent extends BaseEvent {
+    private String accountHolder;
+    private AccountType accountType;
+    private Date createdDate;
+    private double openingBalance;
+}
